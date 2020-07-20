13126e94358eaa8535b5265da409fde6690dcd95
villemie <63785132+villemie@users.noreply.github.com>
2020-07-20 17:27:13 +0300

    Finished knight rider light for arduino
    Uses now 3 leds at the time

--- knightRider.ino
+++ knightRider.ino
@@ -7,21 +7,31 @@ void setup() {
 }
 
 void loop() {
-   for (int thisPin = 6; thisPin < 12; thisPin++) {
+   for (int thisPin = 6; thisPin < 13; thisPin++) {
     // turn the pin on:
     digitalWrite(thisPin, HIGH);
     delay(timer);
     // turn the pin off:
-    digitalWrite(thisPin, LOW);
+    if(thisPin-2 > 5){
+      digitalWrite(thisPin-2, LOW);
+    }
   }
-  
-  // loop from the highest pin to the lowest:
+  delay(timer);
+  digitalWrite(11, LOW);
+  delay(timer);
+    // loop from the highest pin to the lowest:
   for (int thisPin = 12; thisPin >= 6; thisPin--) {
     // turn the pin on:
     digitalWrite(thisPin, HIGH);
     delay(timer);
     // turn the pin off:
-    digitalWrite(thisPin, LOW);
+    if(thisPin+2 < 13){
+      digitalWrite(thisPin+2, LOW);
+    }
+    
   }
+  delay(timer);
+  digitalWrite(7, LOW);
+  delay(timer);
 
 }
