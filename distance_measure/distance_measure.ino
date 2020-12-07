385488d3eefa7fa73da940a240a1d12d8107edb8
villemie <63785132+villemie@users.noreply.github.com>
2020-12-07 20:11:01 +0200

    Update distance_measure.ino
    Distance measuring can now be started only by pressing a button

--- distance_measure/distance_measure.ino
+++ distance_measure/distance_measure.ino
@@ -1,24 +1,35 @@
+//Uses ezButton library https://arduinogetstarted.com/tutorials/arduino-button-library
+#include <ezButton.h>
 const int trigPin = 2;
 const int echoPin = 3;
-
+ezButton btn(4); //Pin 4 is connected to button which triggers measuring
 float duration, distance;
 
 void setup() {
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+btn.loop();
+if(btn.isPressed()){
+    measure();     
+  }
+   delay(10);
+}
+
+void measure(){
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
   duration = pulseIn(echoPin, HIGH);
   distance = (duration*.0343)/2;
+  distance = ((int) (distance * 10.0 + 0.5) / 10.0);
   Serial.print("Distance: ");
-  Serial.println(distance);
-  delay(1000);
-}
+  Serial.println(distance,1);
+  delay(10);
+  }
